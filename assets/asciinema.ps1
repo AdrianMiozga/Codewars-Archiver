a930f7ed2d8cca555f25d33109d351dabcecabbd
Adrian Miozga <AdrianMiozga@outlook.com>
2023-11-27 23:08:29 +0100

    Add assets

---
+++ assets/asciinema.ps1
@@ -0,0 +1,8 @@
+agg `
+    --font-dir "C:\Windows\Fonts" `
+    --font-family "Cascadia Code" `
+    --font-size 32 `
+    --cols 80 `
+    --rows 19 `
+    "$PWD\recording.cast" `
+    "$PWD\recording.gif"
